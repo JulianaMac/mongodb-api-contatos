46b7b6102a48a950299724c71d2b30367a6e7b63
JulianaMac <juliana.s.machado@hotmail.com>
2019-11-19 22:04:48 -0300

    first commit

---
+++ src/controller/contatosController.js
@@ -0,0 +1,83 @@
+const contatosCollection = require("../model/contatoSchema")
+
+const getAll = (request, response) => {
+  console.log(request.url)
+  contatosCollection.find((error,contatos) => {
+    if (error){
+      return response.status(500).send(error)
+    } else {
+      return response.status(200).send(contatos)
+    }
+  })
+};
+
+const add = (request, response) => {
+  //novo objeto para nossa coleção
+  const contatoDoBody = request.body
+  const contato = new contatosCollection(contatoDoBody)
+
+  contato.save((error) => {
+    // if(error !== null && error !== undefined)
+    if (error) {
+      return response.status(400).send(error)
+    } else {
+      return response.status(200).send(contato)
+    }
+  })
+  // let contato = request.body
+  // let baseDados = model.agenda.contatos
+  // contato.id = Math.random().toString(36).substr(-8)
+
+  // if (!contato.nome || !contato.dataNascimento || !contato.celular) {
+  //   response.status(400).send("Dados inválidos");
+  // } else {
+  //   if (baseDados.find(dado => dado.nome === contato.nome)) {
+  //     response.status(400).send("Contato já cadastrado")
+  //   } else {
+  //     model.agenda.contatos.push(contato)
+  //     response.status(201).send(contato)
+  //   }
+  // }
+
+}
+
+const getByName = (request, response) => {
+
+  const nomeParam = request.params.nome
+  // buscar nome com filtro utilizando regex (like)
+  const regex = new RegExp(nomeParam)
+  const filtro = { nome : regex }
+
+  // const filtro = {nome: nomeParam} - buscar somente o nome sem filtro
+
+  contatosCollection.find(filtro, (error, contatos) => {
+    if (error) {
+      return response.status(500).send(error)
+    } else{
+      return response.status(200).send(contatos)
+    }
+  })
+}
+
+const getById = (request, response) => {
+  const idParam = request.params.id
+  contatosCollection.findById(idParam, (error, contato) => {
+    if (error) {
+      return response.status(500).send(error)
+    } else {
+      if (contato){
+        return response.status(200).send(contato)
+      } else {
+        return response.status(404).send('Contato não encontrado!')
+      }
+    }
+    })
+}
+
+
+module.exports = {
+  getAll,
+  add,
+  getByName,
+  getById
+}
---
+++ src/routes/contatosRoute.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const bodyParser = require("body-parser")
+const controller = require("../controller/contatosController")
+
+router.get("/", controller.getAll)
+router.get("/nome/:nome", controller.getByName)
+router.get("/:id", controller.getById)
+router.post("/criar", bodyParser.json(), controller.add)
+
+module.exports = router
